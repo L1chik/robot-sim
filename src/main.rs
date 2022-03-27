092dbbbaa06975be5ded1e0a51dafb399e522667
Gleb Vlasov <l1chik@pop-os.localdomain>
2022-03-27 22:40:39 +0300

    testing ray intersection

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use kiss3d::camera::{ArcBall, Camera};
 use kiss3d::event::{Action, MouseButton, WindowEvent};
 use kiss3d::light::Light;
 use kiss3d::window::Window;
-use na::{Point2, Point3, Translation2, Vector2, Vector3};
-use ncollide3d::query::{Ray, RayIntersection};
+use na::{Isometry3, Point2, Point3, Translation2, Vector2, Vector3};
+use ncollide3d::query::{Ray, RayCast, RayIntersection};
+use ncollide3d::shape::{Cuboid, TriMesh};
 
 
 pub fn draw_ray(ray: Ray<f32>, window: &mut Window) {
@@ -28,14 +29,20 @@ fn main() {
     let eye = Point3::new(2.0, 2.0, 2.0);
     let at = Point3::origin();
     let mut arc_ball = ArcBall::new(eye, at);
+    arc_ball.rebind_drag_button(Option::from(MouseButton::Button3));
 
     //Object settings
-    window.add_cube(1.0, 1.0, 1.0);
+    // let mut cube = window.add_cube(1.0, 1.0, 1.0);
+    let cube = ncollide3d::procedural::cuboid(&Vector3::new(1.0f32, 1.0, 1.0));
+    window.add_trimesh(cube, Vector3::new(1.0, 1.0, 1.0));
+    let mut ray = Ray::new(Point3::new(0.0f32, 0.0, -1.0), Vector3::z());
 
+    // window.add
+    assert!(cube.intersects_ray(&Isometry3::identity(), &ray, std::f32::MAX));
     //Ray settings
     let mut last_pos = Point2::new(0.0, 0.0);
     let sel_pos = Vector3::new(0.0, 0.0, 0.0);
-    let mut ray = Ray::new(Point3::new(0.0, 0.0, 0.0), sel_pos);
+    // let mut ray = Ray::new(Point3::new(0.0, 0.0, 0.0), sel_pos);
 
     while window.render_with_camera(&mut arc_ball) {
         draw_ray(ray, &mut window);
